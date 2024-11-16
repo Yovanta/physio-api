9064acfbc96d937a4dd80d2e11fdb38130e76ae3
Yovanta Anjelina <yovantaanjelina1001@gmail.com>
2024-11-17 00:40:37 +0700

    initial commit

---
+++ src/main/java/com/enigma/physioapi/PhysioApiApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.physioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhysioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhysioApiApplication.class, args);
+	}
+
+}
